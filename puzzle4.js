3296e6e5749b027a04b42bcc7fc0e7538af7fd02
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-10 16:31:01 -0700

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,20 @@
+down
+down
+down
+down
+right
+get the color
+save the color 
+down
+set the color
+right
+right
+up
+right
+get the color
+save the color 
+down
+set the color 
+right 
+right
+get the dots
