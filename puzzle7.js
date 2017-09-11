33cb7c61c2c7d1e85cf6efd09ae5511f586240ed
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-10 17:06:55 -0700

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,18 @@
+recipe no.1
+repeat if getColor isn't red
+|down
+if getColor is red
+|right
+|right
+repeat if getColor isn't red
+|up
+if getColor is red
+|right
+|right
+no.1
+no.1
+repeat if getColor isn't red
+|down
+if getColor is red
+|red
+get the dots
