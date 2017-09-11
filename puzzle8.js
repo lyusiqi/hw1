b50daf28bd8870ea12d1d68b668471b6a98600af
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-10 17:28:14 -0700

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,21 @@
+recipe StepFoward
+save 0 as steps
+repeat if steps < 9
+| down
+| save steps + 1 as steps
+|right
+|right
+save 0 as steps
+repeat if steps < 9
+|up
+|save steps + 1 as steps
+|right
+|right
+StepFoward
+StepFoward
+save 0 as steps
+repeat if steps < 8
+|down
+|save steps + 1 as steps
+right
+get the dots
