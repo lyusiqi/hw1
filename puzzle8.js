c8c20e5d3026f35ba2f10486de68868fed0b4517
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-13 10:43:37 -0700

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -1,21 +1,27 @@
-recipe StepFoward
-save 0 as steps
-repeat if steps < 9
-| down
-| save steps + 1 as steps
-|right
-|right
-save 0 as steps
-repeat if steps < 9
-|up
-|save steps + 1 as steps
-|right
-|right
-StepFoward
-StepFoward
-save 0 as steps
-repeat if steps < 8
-|down
-|save steps + 1 as steps
+recipe Down
+  |save 0 as steps
+  |repeat if steps < 9
+    | down
+    | save steps + 1 as steps
+recipe Right
+  |right
+  |right
+recipe Up
+  |save 0 as steps
+  |repeat if steps < 9
+    |up
+    |save steps + 1 as steps
+recipe Get 
+  |Down
+  |Right
+  |Up
+  |Right
+Get
+Get
+recipe Down2
+  |save 0 as steps
+  |repeat if steps < 8
+    |down
+    |save steps + 1 as steps
+Down2
 right
-get the dots
