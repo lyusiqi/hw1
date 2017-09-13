a3b9bbb46a5d139811e0aef71861111a8296f7c5
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-13 09:07:42 -0700

    Update puzzle5.js

--- puzzle5.js
+++ puzzle5.js
@@ -1,14 +1,13 @@
 recipe Colorchoice
 |right
 |right
-if getColor is blue
-|down
-if getColor is red
-|up
+|if getColor is blue
+  |down
+|if getColor is red
+  |up
 Colorchoice
 Colorchoice
 Colorchoice
 right
 right
 right
-get the dots
